e80016ad977695216c134ef39bc9f29bf0acb6df
angelwayar <angelmanuelwayar@gmail.com>
2024-07-18 17:46:23 -0400

    update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FileModule } from './modules/files/file.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 
 
@@ -10,7 +10,13 @@ import { MongooseModule } from '@nestjs/mongoose';
   imports: [
     FileModule,
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRoot('STR_CONNECTION'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('STR_CONNECTION'),
+      }),
+      inject: [ConfigService],
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
